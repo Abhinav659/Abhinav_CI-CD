429dcd3d86861aafd407123594faea0da4f46ccc
Abhinav659 <73571966+Abhinav659@users.noreply.github.com>
2024-01-19 00:26:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -16,11 +16,11 @@ window.addEventListener("mousemove", (e) => {
     cursorDot.style.left = `${posX}px`;
     cursorDot.style.top = `${posY}px`;
 
-    cursorOutline.style.left = `${posX}px`;
-    cursorOutline.style.top = `${posY}px`;
+    // cursorOutline.style.left = `${posX}px`;
+    // cursorOutline.style.top = `${posY}px`;
 
-    // cursorOutline.animate({
-    //     left: `${posX}px`,
-    //     top: `${posY}px`
-    // }, { duration: 500, fill: "forwards" });
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`
+    }, { duration: 500, fill: "forwards" });
 });
