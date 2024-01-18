e5d4dff3de6ea556b5e204627a9c1f7ad56e5b94
Abhinav Sharma <abhinavsharma159@gmail.com>
2024-01-18 16:59:58 +0530

    upload files

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["DevOps Engineer", "Cloud Engineer", "Full Stack Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+ });
+ 
