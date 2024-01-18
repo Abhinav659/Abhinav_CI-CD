61c8e47fe6de94a8954c9de749e131d6a2436010
Abhinav659 <73571966+Abhinav659@users.noreply.github.com>
2024-01-19 00:03:23 +0530

    Added smooth cursor follower logic

--- main.js
+++ main.js
@@ -5,4 +5,22 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true
  });
- 
+
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", (e) => {
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    // cursorOutline.style.left = `${posX}px`;
+    // cursorOutline.style.top = `${posY}px`;
+
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`
+    }, { duration: 500, fill: "forwards" });
+});
